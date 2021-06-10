0d6ddf0b00ecaff14ba797e4c6fd0e32c742d8e0
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-06-10 11:15:30 -0300

    refactor: move from_dict_generator to new file

--- rutyva/base_model.py
+++ rutyva/base_model.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 from dataclasses import dataclass, asdict
-from typing import Any, Literal, Union, get_args, get_origin
 from .validation import validate_any
+from .from_dict_generator import gen_from_any
 
 @dataclass
 class BaseModel:
@@ -42,96 +42,9 @@ class BaseModel:
       if key not in dc_fields:
         raise TypeError(f'{key} is not an attribute of {cls.__name__}')
 
-      d_copy[key] = cls._gen_from_any(d_copy[key], dc_fields[key].type)
+      d_copy[key] = gen_from_any(d_copy[key], dc_fields[key].type)
       
     return cls(**d_copy) # type: ignore
-
-  @staticmethod
-  def _gen_from_any(d_val, ann):
-    origin = get_origin(ann)
-    if origin is not None:
-      if origin == list:
-        return BaseModel._gen_from_list(d_val, get_args(ann)[0])
-      if origin == Union:
-        return BaseModel._gen_from_union(d_val, get_args(ann))
-      if origin == Literal: #(int, bool, str, bytes, None)
-        return d_val
-      if origin == tuple:
-        return BaseModel._gen_from_tuple(d_val, get_args(ann))
-      if origin == dict:
-        return BaseModel._gen_from_dict(d_val, get_args(ann))
-
-      print('GENERATION WITH ORIGIN NOT IMPLEMENTED:', ann, origin, d_val)
-      return d_val
-    else:
-      if isinstance(d_val, ann):
-        return d_val
-      if issubclass(ann, BaseModel):
-        return ann.from_dict(d_val) 
-      if ann == Any:
-        return d_val
-      
-      try:
-        return ann(**d_val)
-      except:
-        return d_val
-
-      # print('GENERATION NOT IMPLEMENTED:', ann, d_val)
-  
-  @staticmethod
-  def _gen_from_list(d_val, ann):
-    if not isinstance(d_val, list): return d_val
-
-    gen_list = []
-    for obj in d_val:
-      gen_list.append(BaseModel._gen_from_any(obj, ann))
-    
-    return gen_list
-
-  @staticmethod
-  def _gen_from_tuple(d_val, anns):
-    if not isinstance(d_val, (tuple, list)): return d_val
-
-    anns_len = len(anns)
-    d_val_len = len(d_val)
-
-    if anns_len != d_val_len: return d_val
-
-    gen_lis = []
-    for i in range(len(anns)):
-      gen_lis.append(BaseModel._gen_from_any(d_val[i], anns[i]))
-
-    return tuple(gen_lis)
-
-  @staticmethod
-  def _gen_from_union(d_val, anns):
-    for ann in anns:
-      try:
-        new_val = BaseModel._gen_from_any(d_val, ann)
-      except:
-        new_val = d_val
-      
-      if d_val != new_val:
-        return new_val
-    
-    return d_val
-  
-  @staticmethod
-  def _gen_from_dict(d_val, amns):
-    if not isinstance(d_val, dict): return d_val
-    new_dict = {}
-    key_class = amns[0]
-    val_class = amns[1]
-
-    for val in d_val:
-      new_key = BaseModel._gen_from_any(val, key_class) #needs to be hashable (int, bool, str, tuple)
-      new_val = BaseModel._gen_from_any(d_val[val], val_class)
-      try:
-        new_dict[new_key] = new_val
-      except:
-        return d_val
-
-    return new_dict
 
     #list -> get_list=None, get_args=()
     #list[Any] -> get_origin=list, get_args=(Any,)
