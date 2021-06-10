21bcbccd8b36d117beb7ba8de4f51e3afee6b84c
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-06-10 15:01:22 -0300

    test: add some tests

--- tests/test_from_dict_generator.py
+++ tests/test_from_dict_generator.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import unittest
 from rutyva import BaseModel
 from dataclasses import dataclass
@@ -6,12 +7,39 @@ from rutyva.from_dict_generator import gen_from_any
 class TestFromDictGen(unittest.TestCase):
   def test_inner_BaseModel(self):
     @dataclass
-    class TestClass(BaseModel):
+    class TestInnerClass(BaseModel):
       a: int
+    
+    @dataclass
+    class TestOuterClass(BaseModel):
+      i: TestInnerClass
 
-    d_val = {'a' : 1}
-    result = gen_from_any(d_val, TestClass)
+    d_val = {'i': {'a' : 1}}
+    result = TestOuterClass.from_dict(d_val)
 
+  def test_inner_Optional_BaseModel(self):
+    @dataclass
+    class TestInnerClass(BaseModel):
+      a: int
+    
+    @dataclass
+    class TestOuterClass(BaseModel):
+      i: Optional[TestInnerClass]=None
+
+    d_val = {'i': {'a' : 1}}
+    result = TestOuterClass.from_dict(d_val)
+
+  def test_inner_Optional_BaseModel_None(self):
+    @dataclass
+    class TestInnerClass(BaseModel):
+      a: int
+    
+    @dataclass
+    class TestOuterClass(BaseModel):
+      i: Optional[TestInnerClass]=None
+
+    d_val = {}
+    result = TestOuterClass.from_dict(d_val)
  
 if __name__=="__main":
   unittest.main()
