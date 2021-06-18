f0d5c6e6a02ade8295b1878535e9f3e183748137
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-06-18 16:38:28 -0300

    feat: add subclass substitution for from dict generation

--- rutyva/base_model.py
+++ rutyva/base_model.py
@@ -1,7 +1,8 @@
 from copy import deepcopy
 from dataclasses import dataclass, asdict
+from typing import Optional
 from .validation import validate_var
-from .from_dict_generator import gen_from_any
+from .from_dict_generator import check_subclasses, gen_from_any
 
 
 @dataclass
@@ -35,8 +36,10 @@ class BaseModel:
     if not raise_error: return True
 
   @classmethod
-  def from_dict(cls, d: dict, raise_if_extra_attribute=False):
+  def from_dict(cls, d: dict, raise_if_extra_attribute=False, subclass_subst: Optional[list[tuple[type, type]]]=None):
     if not isinstance(d, dict): raise TypeError('Input is not a dict!')
+    if subclass_subst is not None:
+      check_subclasses(subclass_subst)
     d_copy = deepcopy(d)
     d_return = dict()
 
@@ -48,7 +51,7 @@ class BaseModel:
       else:
         if dc_fields[key].init:
           try:
-            d_return[key] = gen_from_any(d_copy[key], dc_fields[key].type)
+            d_return[key] = gen_from_any(d_copy[key], dc_fields[key].type, subclass_subst)
           except Exception as e:
             error_message = f'({cls.__name__}) attribute ({key}) of type ' + e.args[0]
             raise TypeError(error_message)
--- rutyva/from_dict_generator.py
+++ rutyva/from_dict_generator.py
@@ -1,33 +1,39 @@
-from typing import get_origin, get_args, Union, Literal, Any
-from copy import deepcopy
+from typing import Optional, get_origin, get_args, Union, Literal, Any
 
 
-def from_dict(cls, d: dict):
-  if not isinstance(d, dict): raise TypeError('Input is not a dict!')
-  d_copy = deepcopy(d)
+def get_subclass_if_in_list(parent_class: type, subclass_subst: Optional[list[tuple[type, type]]]):
+  if subclass_subst is None: return parent_class
+  filtered_class = [t[1] for t in subclass_subst if t[0] == parent_class]
+  if len(filtered_class) == 0: return parent_class
+  return filtered_class[0]
 
-  dc_fields = cls.__dataclass_fields__  # type: ignore
-  for key in d_copy:
-    if key not in dc_fields:
-      raise TypeError(f'{key} is not an attribute of {cls.__name__}')
+def check_subclasses(subclasses_subs: list[tuple[type, type]]):
+  for class_pair in subclasses_subs:
+    raise_if_is_not_type(class_pair[0])
+    raise_if_is_not_type(class_pair[1])
+    raise_if_not_subclass(class_pair[0], class_pair[1])
 
-    d_copy[key] = gen_from_any(d_copy[key], dc_fields[key].type)
-    
-  return cls(**d_copy) # type: ignore
+def raise_if_not_subclass(parent_cls: type, child_cls: type):
+  if not issubclass(child_cls, parent_cls):
+    raise TypeError(f'{child_cls.__name__} is not a subclass of {parent_cls.__name__}')
 
-def gen_from_any(d_val, ann):
+def raise_if_is_not_type(cls):
+  if not isinstance(cls, type):
+    raise TypeError(f'{cls} is not a class!')
+
+def gen_from_any(d_val, ann, subclass_subst):
   origin = get_origin(ann)
   if origin is not None:
     if origin == list:
-      return gen_from_list(d_val, get_args(ann)[0])
+      return gen_from_list(d_val, get_args(ann)[0], subclass_subst)
     if origin == Union:
-      return gen_from_union(d_val, get_args(ann))
+      return gen_from_union(d_val, get_args(ann), subclass_subst)
     if origin == Literal: #(int, bool, str, bytes, None)
       return d_val
     if origin == tuple:
-      return gen_from_tuple(d_val, get_args(ann))
+      return gen_from_tuple(d_val, get_args(ann), subclass_subst)
     if origin == dict:
-      return gen_from_dict(d_val, get_args(ann))
+      return gen_from_dict(d_val, get_args(ann), subclass_subst)
 
     print('GENERATION WITH ORIGIN NOT IMPLEMENTED:', ann, origin, d_val)
     return d_val
@@ -38,25 +44,26 @@ def gen_from_any(d_val, ann):
       return d_val
 
     # check if has from_dict class method
-    from_dict_method = getattr(ann, 'from_dict', None)
+    subst_subclass_or_class = get_subclass_if_in_list(ann, subclass_subst)
+    from_dict_method = getattr(subst_subclass_or_class, 'from_dict', None)
     if from_dict_method is not None:
       return from_dict_method(d_val)
     
     try:
-      return ann(**d_val)
+      return subst_subclass_or_class(**d_val)
     except:
       return d_val
 
-def gen_from_list(d_val, ann):
+def gen_from_list(d_val, ann, subclass_subst):
   if not isinstance(d_val, list): return d_val
 
   gen_list = []
   for obj in d_val:
-    gen_list.append(gen_from_any(obj, ann))
+    gen_list.append(gen_from_any(obj, ann, subclass_subst))
   
   return gen_list
 
-def gen_from_tuple(d_val, anns):
+def gen_from_tuple(d_val, anns, subclass_subst):
   if not isinstance(d_val, (tuple, list)): return d_val
 
   anns_len = len(anns)
@@ -66,14 +73,14 @@ def gen_from_tuple(d_val, anns):
 
   gen_lis = []
   for i in range(len(anns)):
-    gen_lis.append(gen_from_any(d_val[i], anns[i]))
+    gen_lis.append(gen_from_any(d_val[i], anns[i], subclass_subst))
 
   return tuple(gen_lis)
 
-def gen_from_union(d_val, anns):
+def gen_from_union(d_val, anns, subclass_subst):
   for ann in anns:
     try:
-      new_val = gen_from_any(d_val, ann)
+      new_val = gen_from_any(d_val, ann, subclass_subst)
     except:
       new_val = d_val
     
@@ -82,15 +89,15 @@ def gen_from_union(d_val, anns):
   
   return d_val
 
-def gen_from_dict(d_val, amns):
+def gen_from_dict(d_val, amns, subclass_subst):
   if not isinstance(d_val, dict): return d_val
   new_dict = {}
   key_class = amns[0]
   val_class = amns[1]
 
   for val in d_val:
-    new_key = gen_from_any(val, key_class) #needs to be hashable (int, bool, str, tuple)
-    new_val = gen_from_any(d_val[val], val_class)
+    new_key = gen_from_any(val, key_class, subclass_subst) #needs to be hashable (int, bool, str, tuple)
+    new_val = gen_from_any(d_val[val], val_class, subclass_subst)
     try:
       new_dict[new_key] = new_val
     except:
